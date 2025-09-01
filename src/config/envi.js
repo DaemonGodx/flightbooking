f986501e16fd265405d82bf4be798458b64c8925
DaemonGodx <pritam.2024ug@iiitranchi.ac.in>
2025-09-01 19:40:48 +0530

    folder structure

---
+++ src/config/envi.js
@@ -0,0 +1,5 @@
+const env=require('dotenv');
+env.config();
+module.exports={
+    PORT:process.env.port, 
+};
---
+++ src/index.js
@@ -0,0 +1,14 @@
+const express=require('express');
+const bodyParser=require('body-parser');
+const {PORT}=require('./config/envi');
+
+async function startServer(){
+    const app=express();
+    app.use(bodyParser.json());
+    app.use(bodyParser.urlencoded({extended:true}));
+
+    app.listen(PORT,()=>{
+        console.log(`Server is running on port ${PORT}`);
+    });
+}
+startServer();
